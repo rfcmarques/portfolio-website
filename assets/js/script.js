e6934d5f1b70356399d9798a636dff18ea2e0eb7
Rui Marques <a.marques1967@gmail.com>
2024-02-17 22:57:20 +0000

    Created the typing effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-// Add custom JavaScript here
+function runTypingEffect() {
+    const text = 'I am Rui Marques';
+    const typingElement = document.getElementById('typingText');
+    const typingDelay = 100;
+
+    typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, delay) {
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(() => { typingElement.textContent += text.charAt(i) }, delay * i);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', runTypingEffect);
